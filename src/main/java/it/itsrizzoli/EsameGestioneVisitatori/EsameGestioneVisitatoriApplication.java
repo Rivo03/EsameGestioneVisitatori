c55387f0496fd86a1e33d1450f9e6209692919da
MattiaRivolta <mattia.rivolta@itsrizzoli.it>
2024-10-16 16:54:44 +0200

    primo commit

---
+++ src/main/java/it/itsrizzoli/EsameGestioneVisitatori/EsameGestioneVisitatoriApplication.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.EsameGestioneVisitatori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameGestioneVisitatoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameGestioneVisitatoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/itsrizzoli/EsameGestioneVisitatori/EsameGestioneVisitatoriApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.EsameGestioneVisitatori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameGestioneVisitatoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
