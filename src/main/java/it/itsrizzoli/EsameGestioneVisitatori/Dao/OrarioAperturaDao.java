252330a7804813f17c9c118f99dd074b4a522170
saladavide003 <davide.sala@itsrizzoli.it>
2024-10-31 14:39:18 +0100

    rimossi import inutilizzati nei Dao

--- src/main/java/it/itsrizzoli/EsameGestioneVisitatori/Dao/OrarioAperturaDao.java
+++ src/main/java/it/itsrizzoli/EsameGestioneVisitatori/Dao/OrarioAperturaDao.java
@@ -1,8 +1,6 @@
 package it.itsrizzoli.EsameGestioneVisitatori.Dao;
 
-
 import it.itsrizzoli.EsameGestioneVisitatori.Model.OrarioApertura;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
